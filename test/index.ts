f8e4aaacb7c036fb80ee964ab4354a27627dc6e6
Nick Adamson <nickadamson@pm.me>
2023-10-16 19:35:10 -0700

    fix: type annotations

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,8 @@
+import { PublicClient } from '@wagmi/core';
 import { http, createPublicClient } from 'viem';
 import { arbitrumGoerli } from 'viem/chains';
 
-export const publicClient = createPublicClient({
+export const publicClient: PublicClient = createPublicClient({
   chain: arbitrumGoerli,
   transport: http(),
 });
