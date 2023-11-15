a87704891b1fa836911a607a6bba842be5c19206
Nick Adamson <nickadamson@pm.me>
2023-11-15 13:14:10 -0800

    fix: unixTimestamp bug where it could return a float

--- src/utils/timestamps.ts
+++ src/utils/timestamps.ts
@@ -1,17 +1,22 @@
 import { ONE_DAY_UNIX } from '../constants';
 
 /**
+ * Convert the UTC date to a unix timestamp,
+ * Casts to integer (via Math.floor) for conversion to BigInts
+ */
+export const toUnix = (date: Date) => Math.floor(date.getTime() / 1000);
+
+/**
  * By aligning to 8AM UTC, we limit fragmentation of liquidity
  *
  * @param date - The date to adjust.
  * @returns The adjusted 8 AM UTC date.
  */
 export function get8AMUTCDate(date: Date) {
-  const today = new Date();
-  // Convert tomorrow's date to its UTC equivalent. This is achieved by adjusting
+  // Convert the date to its UTC equivalent. This is achieved by adjusting
   // the local date with the timezone offset in milliseconds.
-  const tzOffsetMS = today.getTimezoneOffset() * 60000;
-  const utcDateMilliseconds = today.getTime() + tzOffsetMS;
+  const tzOffsetMS = date.getTimezoneOffset() * 60000;
+  const utcDateMilliseconds = date.getTime() + tzOffsetMS;
 
   // Create a new Date object from the UTC milliseconds.
   const utcDate = new Date(utcDateMilliseconds);
@@ -23,16 +28,17 @@ export function get8AMUTCDate(date: Date) {
   // the 8AM date. Otherwise, return 8AM of the following day. This ensures that
   // we're always returning a future date relative to the original input.
   if (date.getTime() >= utcDate.getTime()) {
-    return (utcDate.getTime() + ONE_DAY_UNIX) / 1000; // Convert the UTC date to a unix timestamp
+    utcDate.setDate(utcDate.getDate() + 1); // Add a day to the UTC date
   }
-  return utcDate.getTime() / 1000; // Convert the UTC date to a unix timestamp
+
+  return toUnix(utcDate);
 }
 
-export function get24HrTimestamps(): {
+export function get24HrTimestamps(date?: Date): {
   exerciseTimestamp: number;
   expiryTimestamp: number;
 } {
-  const exerciseTimestamp = get8AMUTCDate(new Date());
+  const exerciseTimestamp = get8AMUTCDate(date ?? new Date());
   const expiryTimestamp = exerciseTimestamp + ONE_DAY_UNIX; // expires in 1 day
 
   return { exerciseTimestamp, expiryTimestamp };
