5e9ca882915ed39a99d05054bd86f3956158b420
Nick Adamson <nickadamson@pm.me>
2023-11-16 17:57:56 -0800

    chore: swap tsup for parcel

--- src/grpc/index.ts
+++ src/grpc/index.ts
@@ -1,24 +1,4 @@
-export type {
-  AuthClient,
-  FeesClient,
-  RFQClient,
-  SpotClient,
-  ValoremGRPCClients,
-} from './clients';
-export {
-  toH40,
-  toH96,
-  toH128,
-  toH160,
-  toH256,
-  fromH40,
-  fromH96,
-  fromH128,
-  fromH160,
-  fromH160ToAddress,
-  fromH256,
-  parseQuoteResponse,
-  type ParsedQuoteResponse,
-} from './hi-lo-bit-segmentation';
-export { trackCookieInterceptor } from './interceptors';
-export { handleGRPCRequest } from './utils';
+export * from './clients';
+export * from './hi-lo-bit-segmentation';
+export * from './interceptors';
+export * from './utils';
--- tsup.cjs.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-import { baseTsupConfig } from './tsup.esm';
-
-export default defineConfig({
-  ...baseTsupConfig,
-  format: ['cjs'],
-  outDir: 'dist/_cjs',
-  sourcemap: true,
-});
