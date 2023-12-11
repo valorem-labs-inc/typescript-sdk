8bd1018ce1d337334f5a4a05d4d43be8d736538d
Nick Adamson <nickadamson@pm.me>
2023-12-11 10:46:13 -0800

    chore: add declaration file for math.js/number

---
+++ mathjs.number.d.ts
@@ -0,0 +1,10 @@
+declare module 'mathjs/number';
+
+declare function abs(x: number): number
+declare function erf(x: number): number
+declare function exp(x: number): number
+declare function log(x: number): number
+declare function max(...values: number[]): number
+declare const pi: number
+declare function pow(base: number, exponent: number): number
+declare function sqrt(x: number): number
