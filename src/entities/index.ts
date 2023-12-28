21728f2f542a9131c17f9b93d6e4afcc3effa2aa
Nick Adamson <nickadamson@pm.me>
2023-12-27 17:05:38 -0800

    🚨 (chore): prettier

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,28 @@
-export { Asset, type ERC20Token, OptionAssetPair, SupportedAsset, SUPPORTED_ASSETS } from './assets';
-export { type ContractConstructorArgs, type IClearinghouse, type IERC20, type ISeaport, type ISeaportValidator, ClearinghouseContract, ERC20Contract, SeaportContract, SeaportValidatorContract } from './contracts';
+export {
+  Asset,
+  type ERC20Token,
+  OptionAssetPair,
+  SupportedAsset,
+  SUPPORTED_ASSETS,
+} from './assets';
+export {
+  type ContractConstructorArgs,
+  type IClearinghouse,
+  type IERC20,
+  type ISeaport,
+  type ISeaportValidator,
+  ClearinghouseContract,
+  ERC20Contract,
+  SeaportContract,
+  SeaportValidatorContract,
+} from './contracts';
 export { Maker, Taker, WebTaker } from './trader';
-export { OptionType, type OptionTypeArgs, Option, type OptionArgs, Claim, type ClaimArgs, SubgraphPosition } from './options';
+export {
+  OptionType,
+  type OptionTypeArgs,
+  Option,
+  type OptionArgs,
+  Claim,
+  type ClaimArgs,
+  SubgraphPosition,
+} from './options';
